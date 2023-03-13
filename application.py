1dc8214f9a8beedf2b0de84c7b67983f6405a993
Sourabh Yadav <saurav_sori@yahoo.com>
2023-03-13 21:55:33 +0000

    applicant update

--- application.py
+++ application.py
@@ -10,9 +10,13 @@ def lambda_handler(event, context):
     #n4 = event['age']
     #print(n1)
     #print(context)
-
+    if n2 == 'lname':
+        state = 'lname'
+    else:
+        state = 'fname'
     sf = boto3.client('stepfunctions', region_name = 'eu-west-2')
-    input_dict = {'type': n2, 'name': n1}
+    input_dict = {'state': state, 'name': n1}
     response = sf.start_execution(
     stateMachineArn = 'arn:aws:states:eu-west-2:918781784251:stateMachine:sourabhfsm',
-    input = json.dumps(input_dict))    
+    input = json.dumps(input_dict))
+    return input_dict
